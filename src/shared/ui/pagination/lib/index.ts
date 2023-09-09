0be1222165dc1207eec229c803c33744f2b53af9
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-09-09 19:54:01 +0300

    Updating pagination without comments

--- src/shared/ui/pagination/lib/index.ts
+++ src/shared/ui/pagination/lib/index.ts
@@ -1,3 +1,4 @@
+// Custom pagination
 export const defineNumberOfPages = (dataLength: number, pageLimit: number, currentPage: number) => {
   const numberOfPages: number | undefined = dataLength && Math.ceil(dataLength / pageLimit);
   const arrayOfPages: number[] = [];
@@ -8,10 +9,12 @@ export const defineNumberOfPages = (dataLength: number, pageLimit: number, curre
     const newArrayOfPages: number[] = [];
     arrayOfPages.forEach(
       pageNumber => (
-        pageNumber === 1
-        || pageNumber === arrayOfPages.length
-        || (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2)
-      ) && newArrayOfPages.push(pageNumber)
+        pageNumber === 1 // If this is the first page (1)
+        || pageNumber === arrayOfPages.length // Or if this is the last page (2)
+        || (currentPage < 4
+          ? (pageNumber >= currentPage - 2 && pageNumber < 7) // Or if the current page is less than 4, then increase the right border
+          : (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2))  // Or this page is within +/- 2 pages of the current page (3)
+      ) && newArrayOfPages.push(pageNumber) // Then we add it
     )
 
     return newArrayOfPages;
