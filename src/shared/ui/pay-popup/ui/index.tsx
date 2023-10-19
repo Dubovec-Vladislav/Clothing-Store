a49d5866b6c288adaef5378fde6a3ea777112109
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-10-19 11:39:07 +0300

    Updating name validation on payment form
    Creating a filter only for the latin alphabet, removing extra spaces, creating a pattern for two words, transforming everything into capital letters

--- src/shared/ui/pay-popup/ui/index.tsx
+++ src/shared/ui/pay-popup/ui/index.tsx
@@ -2,6 +2,7 @@
 import React, { ChangeEvent, FC, useState } from "react";
 import style from "./index.module.scss";
 import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
 // Components
 import { Button } from "shared/ui/button";
 import { PaySuccessLoader } from "shared/loaders/pay-success-loader";
@@ -34,13 +35,6 @@ export const PayPopup: FC<PayPopupProps> = ({ toggleIsPopupActive }) => {
     formState: { errors },
   } = useForm({ mode: "onTouched" });
 
-  // const { mask } = useMask({
-  //   name: "phone",
-  //   mask: "+7 (999) 999-99-99",
-  // });
-
-  const [value, setValue] = useState("");
-
   return (
     <section className={style.block}>
       <div className={style.body}>
@@ -51,22 +45,19 @@ export const PayPopup: FC<PayPopupProps> = ({ toggleIsPopupActive }) => {
         <form className={style.items} onSubmit={handleSubmit(onSubmit)}>
           <div className={style.item}>
             <div className={style.label}>Номер карты</div>
-            <input
+            <InputMask
               className={style.input}
-              type="text"
               placeholder="0123 4567 8901 2345"
+              mask="9999 9999 9999 9999"
+              maskChar=""
               {...register("cardNumber", {
                 required: "Поле с номером обязательно",
                 minLength: {
-                  value: 19,
-                  message: "Введите корректный номер длина",
-                },
-                maxLength: {
-                  value: 19,
-                  message: "Номер карты не должен превышать 16 символов",
+                  value: 19, // 16 number + 3 spaces
+                  message: "Длина номера должна составлять 16 цифр",
                 },
               })}
-            />
+            ></InputMask>
             <div className={style.error}>
               {errors?.cardNumber && String(errors?.cardNumber?.message)}
             </div>
@@ -76,7 +67,6 @@ export const PayPopup: FC<PayPopupProps> = ({ toggleIsPopupActive }) => {
             <div className={style.label}>Имя держателя карты</div>
             <input
               className={style.input}
-              type="text"
               placeholder="BAZAROV OLEG"
               {...register("cardName", {
                 required: "Поле с именем обязательно",
@@ -84,7 +74,32 @@ export const PayPopup: FC<PayPopupProps> = ({ toggleIsPopupActive }) => {
                   value: 16,
                   message: "Введите корректное имя",
                 },
+                pattern: {
+                  value: /^[a-zA-Z]+ [a-zA-Z]+$/,
+                  message: "Введите два слова, разделенных одним пробелом",
+                },
               })}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                const inputValue = e.target.value;
+
+                // We remove all characters except Latin letters and spaces
+                let latinWithSpaceValue = inputValue.replace(/[^a-zA-Z ]/g, "");
+
+                // Check that the first character is not a space
+                if (latinWithSpaceValue[0] === " ")
+                  latinWithSpaceValue = latinWithSpaceValue.slice(1);
+
+                // Checking if there is more than one space
+                const spaceCount = (latinWithSpaceValue.match(/ /g) || [])
+                  .length;
+
+                // If there is more than one space, remove it
+                if (spaceCount > 1)
+                  latinWithSpaceValue = latinWithSpaceValue.slice(0, -1);
+
+                // Write everything in capital letters
+                e.target.value = latinWithSpaceValue.toUpperCase();
+              }}
             />
             <div className={style.error}>
               {errors?.cardName && String(errors?.cardName?.message)}
@@ -93,18 +108,19 @@ export const PayPopup: FC<PayPopupProps> = ({ toggleIsPopupActive }) => {
 
           <div className={style.item}>
             <div className={style.label}>Срок действия</div>
-            <input
+            <InputMask
               className={style.input}
-              type="text"
               placeholder="07/25"
+              mask="99/99"
+              maskChar=""
               {...register("cardData", {
                 required: "Поле со сроком действия обязательно",
-                maxLength: {
-                  value: 4,
-                  message: "Номер карты не должен превышать 16 символов",
+                minLength: {
+                  value: 5, // 4 number + 1 spaces
+                  message: "Дата должна состоять из 4 цифр (месяц/год)",
                 },
               })}
-            />
+            ></InputMask>
             <div className={style.error}>
               {errors?.cardData && String(errors?.cardData?.message)}
             </div>
